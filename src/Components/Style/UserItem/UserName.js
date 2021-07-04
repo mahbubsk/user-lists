6aab4742ef5ab89ea651685c9bab99d1a022512c
mahbubsk <skmahbubkr787@gmail.com>
2021-07-04 21:41:21 +0600

    fix: username margin issue

--- src/Components/Style/UserItem/UserName.js
+++ src/Components/Style/UserItem/UserName.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const UserName = styled.p`
-    margin: 5px 10px 5px 10px;
+    margin: 0 10px;
     font-weight: 400;
     font-size: 16px;
     color: #4A5568;
